e00942cb865acb49396e73e1409dec2c3eca9240
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-02-26 13:54:10 -0500

    Added Home Link to navigation

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ function App() {
       <Layout>
         <Header className="header-color" title="Dhimitris" scroll>
           <Navigation>
+            <Link to="/">Home</Link>
             <Link to="/resume">Resume</Link>
-            <Link to="/aboutme">About Me</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/contact">Contact</Link>
           </Navigation>
         </Header>
         <Drawer title="Dhimitris">
           <Navigation>
+            <Link to="/">Home</Link>
             <Link to="/resume">Resume</Link>
-            <Link to="/aboutme">About Me</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/contact">Contact</Link>
           </Navigation>
